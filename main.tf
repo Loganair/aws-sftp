48f79fd949ded41f5027db979b829229c2474c40
David McCanney <38220805+mccanney@users.noreply.github.com>
2019-05-14 13:23:52 +0100

    Create working server
    - Iniital pass at creating a working server
    - Added admin user with public key

--- main.tf
+++ main.tf
@@ -24,24 +24,45 @@ terraform {
     }
 }
 
-#data "template_file" "role_policy" {
-#    template = "${file("policies/role-policy.json")}"
-#
-#    vars {
-#        uploader   = "${aws_iam_user.fdm_uploader.arn}"
-#        downloader = "${aws_iam_user.fdm_downloader.arn}"
-#        admin      = "${aws_iam_user.fdm_admin.arn}"
-#    }
-#}
+# Import the required IAM policies
+data "template_file" "iam_role_policy" {
+    template = "${file("policies/role-policy.json")}"
+}
 
-#data "template_file" "read_write_policy" {
-#    template = "${file("policies/read-write-policy.json")}"
-#
-#    vars {
-#        bucket = "${aws_s3_bucket.accelya.name}"
-#    }
-#}
+data "template_file" "read_write_policy" {
+    template = "${file("policies/read-write-policy.json")}"
 
+    vars {
+        bucket = "${aws_s3_bucket.accelya.id}"
+    }
+}
+
+# Create the required roles and policies
+resource "aws_iam_role" "accelya_read_write" {
+    name               = "AWSSFTPAccelyaReadWrite"
+    description        = "Provides read/write access to the Accelya S3 bucket via SFTP"
+    assume_role_policy = "${data.template_file.iam_role_policy.rendered}"
+}
+
+resource "aws_iam_role_policy" "accelya_read_write_policy" {
+    name   = "AWSSFTPAccelyaReadWrite-Policy"
+    role   = "${aws_iam_role.accelya_read_write.id}"
+    policy = "${data.template_file.read_write_policy.rendered}"
+}
+
+resource "aws_transfer_user" "logadmin" {
+    server_id = "${aws_transfer_server.loganair-sftp.id}"
+    user_name = "logadmin"
+    role      = "${aws_iam_role.accelya_read_write.arn}"
+}
+
+resource "aws_transfer_ssh_key" "logadmin_key" {
+    server_id = "${aws_transfer_server.loganair-sftp.id}"
+    user_name = "${aws_transfer_user.logadmin.user_name}"
+    body      = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAmbkJTsNeaCTULT+/3jYpNKmBjqmxTmMDhjxiNpCfOvPwfAWGfoTbYYAcB9hM6EXUEhLcOCUkiXGpMsiTOKlG1Jeyx0BGWmudxOn24zW7M8fERQSNYbGbCwBezpUhEfJii82CvV34rxGLvz2KpfTZzwqfUR0J2gAoCQPtgzueBkimD6Ol86Y42wL7wYfZfMf4Sgdx1RaqWBAJ6f5IgPOv6s8Vp4jIGr3Wn5De4m2SQv7UMP00p3fBsYB3G5POhrbTZU6L/AYQn6U657AlmWekcJNmx+nyORy7K87hFTtw8CBytDtfitbU3xfQAtKYfHNXqU0k6fUqYhfhPKOJlluf2w== rsa-key-20190514"
+}
+
+# Create the S3 bucket for the Accelya files
 resource "aws_s3_bucket" "accelya" {
     bucket = "accelya-sftp"
     acl    = "private"
@@ -51,7 +72,7 @@ resource "aws_s3_bucket" "accelya" {
     }
 }
 
-resource "aws_transfer_server" "name" {
+resource "aws_transfer_server" "loganair-sftp" {
     endpoint_type          = "PUBLIC"
     identity_provider_type = "SERVICE_MANAGED"
     #logging_role           = ""
@@ -62,30 +83,6 @@ resource "aws_transfer_server" "name" {
     }
 }
 
-#resource "aws_transfer_user" "logadmin" {
-#    server_id = "${aws_transfer_server.name.id}"
-#    user_name = "logadmin"
-#    role      = ""
-#}
-#
-#resource "aws_transfer_ssh_key" "logadmin" {
-#    server_id = "${aws_transfer_server.name.id}"
-#    user_name = "${aws_transfer_user.logadmin.user_name}"
-#    body      = ""
-#}
-#
-#resource "aws_transfer_user" "accelya" {
-#    server_id = "${aws_transfer_server.name.id}"
-#    user_name = "accelya"
-#    role      = ""
-#}
-#
-#resource "aws_transfer_ssh_key" "accelya" {
-#    server_id = "${aws_transfer_server.name.id}"
-#    user_name = "${aws_transfer_user.accelya.user_name}"
-#    body      = ""
-#}
-#
 ## Add a record to the domain
 #resource "cloudflare_record" "sftp" {
 #  domain  = "${var.cloudflare_zone}"
